2e827cc9651d0bba6fd3d1b1d15987f91bd634cf
MFASHINGABO Prince <mfashingabop99@gmail.com>
2026-07-23 19:35:03 +0200

    feat: initial commit with modular transaction ledger architecture

---
+++ src/main/java/com/ledger/ledger_settlement_service/LedgerSettlementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ledger.ledger_settlement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerSettlementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerSettlementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ledger/ledger_settlement_service/LedgerSettlementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledger.ledger_settlement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerSettlementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
